badc5b7a6a8c4e971c81e74596a96fea2ef82d13
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-06-16 23:40:25 -0400

    Create a struct for this example In this case create a class cart, that have a static method

--- lazy_evaluation_delegadoy_by.playground/Contents.swift
+++ lazy_evaluation_delegadoy_by.playground/Contents.swift
@@ -1,4 +1,15 @@
 import UIKit
 
+class Cart {
+    let paymentConfig = Cart.loadAndPreparePaymentConfig()
+    func checkout() {
+        if paymentConfig["type"] == "card" {
+            // Do something
+        }
+    }
 
-
+    static func loadAndPreparePaymentConfig() -> [String: String] {
+        print("VERY expensive operation!!!!!")
+        return ["key":"calue"]
+    }
+}
